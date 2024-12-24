07920dcba08b12e7409c8fa462175d72a2b69988
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2024-12-24 12:28:46 +0600

    create event for broadcasting

---
+++ app/Events/SendMessage.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SendMessage
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $data = ['asas'];
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return new Channel('user-channel');
+    }
+
+    /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastAs()
+    {
+
+        return 'UserEvent';
+    }
+    /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastWith()
+    {
+
+        return ['title' => 'This notification from artofcse.com'];
+    }
+}
