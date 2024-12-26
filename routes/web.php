ff4eaaa15ae9c0ed946531d664e4b5c3026ace28
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2024-12-26 12:52:50 +0600

    install echo server and configration

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    event(new \App\Events\SendMessage());
+
+    dd('Event Run Successfully.');
 });
